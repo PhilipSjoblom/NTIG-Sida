0d822a57fa625f3c49fcfab4a25a6d230d3912d0
Philip Sjöblom <philip.sjoblom@learnet.se>
2024-10-16 13:10:05 +0200

    Better announcements

--- src/components/announcement.tsx
+++ src/components/announcement.tsx
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react";
 import styles from "./announcement.module.scss";
+import DOMPurify from "dompurify";
 
 export default function Announcement() {
     const [message, setMessage] = useState<string | null>(null);
 
     useEffect(() => {
         if (!process.env.NEXT_PUBLIC_ANNOUNCEMENT_MESSAGE_URL) return;
-        fetch(process.env.NEXT_PUBLIC_ANNOUNCEMENT_MESSAGE_URL)
-            .then(response => response.text())
+        fetch("/e")
+            .then(response => {
+                if (!response.ok) throw new Error("Failed to fetch announcement message");
+                return response.text();
+            })
             .then(setMessage)
             .catch(() => setMessage(null));
     }, []);
@@ -15,7 +19,7 @@ export default function Announcement() {
     return !message ? null : (
         <div className={[styles.announcement, "glass"].join(" ")}>
             <div className="header">Meddelande</div>
-            <div className={styles.announcementBody}>{message}</div>
+            <div className={styles.announcementBody} dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(message) }} />
         </div>
     )
 }
